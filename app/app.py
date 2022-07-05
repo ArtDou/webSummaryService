3877f4837c71456ec6310325abdbf32b89613257
Mathieu GUIGNET <mathieu.guignet@le-campus-numerique.fr>
2022-07-05 16:47:19 +0200

    mise à jour code avec BDD

--- app/app.py
+++ app/app.py
@@ -1,22 +1,55 @@
 from flask import Flask, render_template, request, flash
-import sqlite3
+from flask_sqlalchemy import SQLAlchemy
+
+
 
 app = Flask(__name__)
 
-@app.route("/")
+app.config['SQLACLHEMY_DATABASE_URI']='postgresql://wym_admin:admin@localhost/postgres-db'
+
+db=SQLAlchemy(app)
+
+class Data(db.Model):
+    __tablename__='BDD'
+    id=db.Column(db.Integer, primary_key=True)
+    nom=db.Column(db.String)
+    téléphone=db.Column(db.String)
+    email=db.Column(db.String)
+    commentaire=db.Column(db.String)
+
+    def __init__(self, nom, téléphone, email, commentaire):
+        self.nom=nom
+        self.téléphone=téléphone
+        self.email=email
+        self.commentaire=commentaire
+
+
+@app.route("/home")
 def home():
     return render_template("index.html")
 
-@app.route("/contact")
+@app.route("/contact", methods=['GET', 'POST'])
 def formulaire():
+
+    if request.method =='POST':
+        nom=request.form['nom']
+        téléphone=request.form['téléphone']
+        email=request.form['email']
+        commentaire=request.form['commentaire']
+
+    data=Data(nom, téléphone, email, commentaire)
+    db.session.add(data)
+    db.session.commit()
+
     return render_template("formulaire.html")
 
 @app.route("/about")
 def about():
     return render_template("about.html")
 
-@app.route("/contacted")
-def contacted():
+@app.route("/contacted", methods=['GET'])
+def contacted():  
+
     return "Votre formulaire a bien été envoyé."
 
 #@app.route("/model")
