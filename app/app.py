f8fc1e4a8970dad794792b1edf761f1fa32a3116
irina-piazza <irina@DESKTOP-JLPN0IQ>
2022-07-06 16:09:07 +0200

    add model page irina

--- app/app.py
+++ app/app.py
@@ -1,10 +1,36 @@
 from flask import Flask, render_template, request
+import requests
+
 
 app = Flask(__name__)        
 
 @app.route("/home")
 def home():
     return render_template("index.html")
+
+@app.route("/model", methods=['GET','POST'])
+def model():
+    text_to_summarize = request.form.get('input_text')
+    
+    if request.method == 'POST':
+        model_port= 5000
+        url = f"http://localhost:{model_port}/model/predict"
+        headers = {"Content-Type": "application/json; charset=utf-8","accept": "application/json"}
+        json = {"text":[text_to_summarize]}
+        res = requests.post(url,  headers=headers, json=json)
+        if res.status_code == 200:
+            summary_text = res.text
+        else:   
+            summary_text =f"status_code:{res.status_code}"    
+    else: 
+        summary_text ="no text to summarized"
+    return render_template("model.html", text=text_to_summarize, summary=summary_text)
+
+# @app.route("/result", methods=['GET','POST'])
+# def result():
+#     text='hello world'
+#     summary='Hello'
+#     return render_template("result.html", text=text, summary=summary)
 
 @app.route("/contact", methods=['GET','POST'])
 def formulaire():
@@ -17,9 +43,6 @@ def about():
 @app.route("/contacted")
 def contacted():  
     return "Votre formulaire a bien été envoyé."
-
-#@app.route("/model")
-#def model():
  
 if __name__== '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=8000)
